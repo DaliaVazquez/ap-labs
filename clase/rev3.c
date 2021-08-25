aaa55e57e5aa1dc9cb55345110b4db810cab670c
Dalia <a01635883@itesm.mx>
2021-08-25 00:32:34 +0000

    clase

---
+++ clase/rev3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>                                  
+                                                    
+char* reverse(char *word) {                         
+    char *reversed_word;                            
+    return reversed_word;                           
+}                                                   
+                                                    
+int main(int argc, char **argv){                    
+    // Place your magic here                        
+    char *word;                                     
+                                                    
+    word = argv[1];                                 
+                                                    
+    printf("Original Word: %s \n", word);           
+                                                    
+    printf("Reversed Word: %s\n", reverse(word));   
+    return 0;                                       
+}  
