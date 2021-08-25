937ff2df1f22c3f88a9ede234226adf3ae0e02aa
Dalia <a01635883@itesm.mx>
2021-08-25 02:06:38 +0000

    c2

--- clase/rev3.c
+++ clase/rev3.c
@@ -1,18 +1,32 @@
 #include <stdio.h>                                  
                                                     
-char* reverse(char *word) {                         
-    char *reversed_word;                            
-    return reversed_word;                           
-}                                                   
-                                                    
-int main(int argc, char **argv){                    
-    // Place your magic here                        
-    char *word;                                     
-                                                    
-    word = argv[1];                                 
-                                                    
-    printf("Original Word: %s \n", word);           
-                                                    
-    printf("Reversed Word: %s\n", reverse(word));   
-    return 0;                                       
-}  
+#include <stdio.h>                                   
+#include <string.h>                                  
+                                                     
+                                                     
+char* reverse(char *word) {                          
+    char *reversed_word;                             
+    char tmp;                                        
+    int lenght = strlen(word);                       
+                                                     
+    reversed_word = word;                            
+                                                     
+    for (int i = 0; i < lenght/2; i++) {             
+        tmp = reversed_word[i];                      
+        reversed_word[i] = reversed_word[lenght-i-1];
+        reversed_word[lenght - i - 1]  = tmp;        
+    }                                                
+                                                     
+    return reversed_word;                            
+}                                                    
+                                                     
+int main(int argc, char **argv){                     
+                                                     
+    char *word;                                      
+    word = argv[1];                                  
+                                                     
+    printf("Original Word: %s \n", word);            
+                                                     
+    printf("Reversed Word: %s\n", reverse(word));    
+    return 0;                                        
+}       
