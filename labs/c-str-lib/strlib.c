9e399cd5a93eef0b7a55c818e5f332f90a7ec800
Dalia <a01635883@itesm.mx>
2021-09-28 15:41:51 +0000

    tarea c-str

--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,41 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int i = 0;
+    char c;
+    while(c=str[i]!='\0'){
+        i++;
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *res = origin + mystrlen(origin);
+    while (*addition != '\0'){
+        *res++ = *addition++;
+    }
+
+    *res = '\0';
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
+    int ver = mystrlen(substr);
+    int i = 0;
+    int j = 0;
+    int n = 0;
+
+    while (origin[i] != '\0') {
+        n = i;
+        while (origin[i] != '\0' && origin[i] == substr[j]) {
+            i++;
+            j++;
+        }
+        if (j == ver) {
+            return n;
+        }
+        i++;
+        j = 0;
+    }
     return 0;
 }
