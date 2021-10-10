19b61396b01a09443b47547addde4ffb018002e0
Dalia <a01635883@itesm.mx>
2021-10-10 03:14:31 +0000

    merge

--- labs/c-generic-sort/mergesort.c
+++ labs/c-generic-sort/mergesort.c
@@ -1,4 +1,24 @@
-
-void mergesort(void *lineptr[], int left, int right,
-	       int (*comp)(void *, void *)) {
+void merge(void* lineptr[], int left, int mitad, int right, int (*comp)(void *, void *))
+{	    
+    for (int i=mitad;i<=right;i++)
+    {
+        for(int j=0;j<mitad;j++)
+        {
+            if ((*comp)(lineptr[i],lineptr[j])<0) {
+                void* tmp=lineptr[i];
+                lineptr[i]=lineptr[j]; 
+                lineptr[j]=tmp;
+            }
+        }
+    }
+}
+void mergesort(void *lineptr[], int left, int right,int (*comp)(void *, void *)) {
+	if(left<right)
+    {
+        int mitad=(right+left)/2;
+        mergesort(lineptr,left,mitad,comp);
+        mergesort(lineptr,mitad+1,right,comp);
+		merge(lineptr, left, mitad, right,comp);
+    }   
+    return;
 }
