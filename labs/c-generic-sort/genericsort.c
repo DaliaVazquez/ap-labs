013e5e7e9d450a887d17c6c3cdae5642958aa8cb
Dalia <a01635883@itesm.mx>
2021-10-11 20:08:30 +0000

    tout

--- labs/c-generic-sort/genericsort.c
+++ labs/c-generic-sort/genericsort.c
@@ -29,27 +29,27 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-	FILE * archivo; 
+	FILE * file; 
     char* outputFile=NULL;
-    char* fileToRead=NULL;
-	char *sortType ="no";
+    char* readFile=NULL;
+	char *sort ="no";
 
-	int numeric = 0;
+	int isNum = 0;
 	
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
 
 	if (strcmp(argv[1],"-n")==0){
-        archivo = fopen(argv[2], "r");
-        fileToRead=argv[2];
-		numeric = 1;
+        file = fopen(argv[2], "r");
+        readFile=argv[2];
+		isNum = 1;
     }else{
-        archivo = fopen(argv[1], "r");
-        fileToRead=argv[1];
+        file = fopen(argv[1], "r");
+        readFile=argv[1];
     }
     
-	if (archivo == NULL){
+	if (file == NULL){
        printf("Error: file null  \n");
        return 0;
     }
@@ -61,60 +61,57 @@ int main(int argc, char **argv)
 			}
 			outputFile=argv[i+1];
 		}else if(strcmp(argv[i], "-quicksort")==0){
-			sortType=argv[i];
+			sort=argv[i];
 		}else if(strcmp(argv[i], "-mergesort")==0){
-			sortType=argv[i];
+			sort=argv[i];
 		}
 	}
-    if(strcmp(sortType, "no") == 0){
-        printf("Please input a sort type\n");
+    if(strcmp(sort, "no") == 0){
+        printf("Error: no sort type\n");
         return 0;
 	}
     if(outputFile==NULL) {
 		char* tmp = "sorted_";
-		outputFile = (char*) malloc(strlen(fileToRead)+strlen(tmp)+1);
+		outputFile = (char*) malloc(strlen(readFile)+strlen(tmp)+1);
 		strcpy(outputFile, tmp);
-		strcat(outputFile, fileToRead);
+		strcat(outputFile, readFile);
 	}
 	int lineNum = 0;
-	while((read = getline(&line, &len, archivo)) != -1) {
+	while((read = getline(&line, &len, file)) != -1) {
 		lineNum++;
 	}
 
-	fseek(archivo, 0, SEEK_SET);
-	char **inputArray = (char **)malloc(lineNum*sizeof(char*));
+	fseek(file, 0, SEEK_SET);
+	char **arr = (char **)malloc(lineNum*sizeof(char*));
 	int index = 0;
-	while((read = getline(&inputArray[index], &len, archivo)) != -1) {
+	while((read = getline(&arr[index], &len, file)) != -1) {
 		index++;
 	}
-	fclose(archivo);
+	fclose(file);
 
-	if(strcmp(sortType, "-quicksort") == 0) {
-		printf("... sorting strings.txt file with quicksort\n");
-		if(numeric) {
-			quicksort((void **) inputArray, 0, lineNum-1, (int(*)(void*, void*))(numcmp));
+	if(strcmp(sort, "-quicksort") == 0) {
+		if(isNum) {
+			quicksort((void **) arr, 0, lineNum-1, (int(*)(void*, void*))(numcmp));
 		} else {
-			quicksort((void **) inputArray, 0, lineNum-1, (int(*)(void*, void*))(strcmp));
+			quicksort((void **) arr, 0, lineNum-1, (int(*)(void*, void*))(strcmp));
 		}
-	} else if(strcmp(sortType, "-mergesort") == 0) {
-		printf("... sorting strings.txt file with mergesort\n");
-		if(numeric) {
-			mergesort((void **) inputArray, 0, lineNum-1, (int(*)(void*, void*))(numcmp));
+		printf("sorting strings.txt file with quicksort\n");
+	} else if(strcmp(sort, "-mergesort") == 0) {
+		if(isNum) {
+			mergesort((void **) arr, 0, lineNum-1, (int(*)(void*, void*))(numcmp));
 		} else {
-			mergesort((void **) inputArray, 0, lineNum-1, (int(*)(void*, void*))(strcmp));
+			mergesort((void **) arr, 0, lineNum-1, (int(*)(void*, void*))(strcmp));
 		}
-	} else {
-		printf("Error: The only available algorithms are: -quicksort and -mergsort\n");
-		return 0;
+		printf("sorting strings.txt file with mergesort\n");
 	}
 
-	archivo = fopen(outputFile, "w");
+	file = fopen(outputFile, "w");
 
 	for(int i = 0; i < lineNum; i++) {
-		fprintf(archivo, "%s", inputArray[i]);
+		fprintf(file, "%s", arr[i]);
 	}
 
-	printf("... Results file can be found at %s\n", outputFile);
-	fclose(archivo);
+	printf("Results file can be found at %s\n", outputFile);
+	fclose(file);
     return 0;
 }
