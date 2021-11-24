a16c8ea89d4e808340044b9caefeec911834f310
Dalia <a01635883@itesm.mx>
2021-11-24 05:16:51 +0000

    d

--- labs/go-clockwall/clockWall.go
+++ labs/go-clockwall/clockWall.go
@@ -10,16 +10,21 @@ import (
 )
 
 func cp(des io.Writer, src io.Reader) {
-	if _, err := io.Copy(des, src); err != nil {
+	_, err := io.Copy(des, src)
+
+	if err != nil {
 		log.Fatal(err)
 	}
 }
 func main() {
+
 	if len(os.Args) >= 2 {
 		x := 1
+
 		for _, arg := range os.Args[1:] {
 			log.Println("Port:", arg[strings.LastIndex(arg, "=")+1:])
 			conn, err := net.Dial("tcp", arg[strings.LastIndex(arg, "=")+1:])
+
 			if err == nil {
 				defer conn.Close()
 				go cp(os.Stdout, conn)
@@ -27,11 +32,14 @@ func main() {
 				log.Fatal(err)
 			}
 		}
+
 		estado := make(chan int)
 		x = <-estado
 		log.Println("Close with: ", x)
 		close(estado)
+
 	} else {
+
 		fmt.Println("Incorrect number of arguments")
 		os.Exit(1)
 	}
